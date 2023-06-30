fabfa4b167e7e490e4cd373929b9aab28f4c6bd2
jpducky <34105363+JPDucky@users.noreply.github.com~>
2023-06-29 23:02:15 -0500

    update

--- init.sh
+++ init.sh
@@ -11,12 +11,12 @@ ZSH_DIR=$HOME/.zsh
 
 PKG_MGR=""
 
-PACKAGES=[
+PACKAGES=(
     "neovim"
     "git"
     "curl"
     "zsh"
-]
+)
 
 
 if ! command -v nvim &> /dev/null
@@ -24,7 +24,7 @@ then
     echo "neovim could not be found"
     echo "installing neovim"
     # check for system distro
-    for command in dnf apt yum zypper pamac pacman emerge nixos-rebuild; do
+    for command in dnf apt yum zypper pamac pacman nixos-rebuild; do
         if command -v $command &> /dev/null
         then 
             PKG_MGR=$command
@@ -36,44 +36,43 @@ fi
 
 echo "Package manager $PKG_MGR found!"
 echo "Installing packages now..."
-case $PKG_MGR in
-    dnf)
-        sudo dnf install -y neovim git curl zsh
-        ;;
-    apt)
-        sudo apt install -y neovim git curl zsh
-        ;;
-    yum)
-        sudo yum install -y neovim git curl zsh
-        ;;
-    zypper)
-        sudo zypper install -y neovim git curl zsh
-        ;;
-    pamac)
-        sudo pamac install -y neovim git curl zsh
-        ;;
-    pacman)
-        sudo pacman install -y neovim git curl zsh
-        ;;
-    emerge)
-        sudo emerge -y neovim git curl zsh
-        ;;
-    nixos-rebuild)
-        config_file=/etc/nixos/configuration.nix
-        for package in $PACKAGES; do
-            if ! grep -q $package $config_file; then
-                echo "Installing $package"
-                sudo sed -i "/environment.systemPackages = with pkgs; \[/a \  $package" $config_file
-            fi
-        done
-        sudo nixos-rebuild switch 
-        ;;
-    *)
-        echo "No package manager found!"
-        echo "Please install neovim, git, curl, and zsh"
-        exit 1
-        ;;
-esac
+for package in ${PACKAGES[@]}; do 
+    if ! command -v $package &> /dev/null
+    then 
+        echo "Installing $package"
+        case $PKG_MGR in
+            dnf)
+                sudo dnf install -y $package
+                ;;
+            apt)
+                sudo apt install -y $package
+                ;;
+            yum)
+                sudo yum install -y $package
+                ;;
+            zypper)
+                sudo zypper install -y $package
+                ;;
+            pamac)
+                sudo pamac install -y $package
+                ;;
+            pacman)
+                sudo pacman -Syu -y $package
+                ;;
+            nixos-rebuild)
+                config_file=/etc/nixos/configuration.nix
+                if ! grep -q $package $config_file; then
+                    echo "Installing $package"
+                    sudo sed -i "/environment.systemPackages = with pkgs; \[/a \  $package" $config_file
+                fi
+                sudo nixos-rebuild switch 
+                ;;
+            *)
+                echo "No package manager found!"
+                echo "Please install neovim, git, curl, and zsh"
+                exit 1
+                ;;
+        esac
 echo "Packages installed!"
 echo "Installing dotfiles now..."
 
